14e9d862150deccf8ee95d4683f523fe5f0532f7
AndresHMosqueda <andreshm94@gmail.com>
2019-03-25 17:30:23 -0600

    Back end

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 // Requires 
-
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser')
 
 // inicializar variables
-
 var app = express();
 
-// escuchar peticiones
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+// importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
+// conexion a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+
+if (err) throw err;
+
+console.log('Base datos: \x1b[32m%s\x1b[0m', 'onine');
+
+});
+
+// Rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
+
+
+// escuchar peticiones
 app.listen(3000, ()=>{
     console.log('Express server running on port 3000: \x1b[32m%s\x1b[0m', 'online')
 })
